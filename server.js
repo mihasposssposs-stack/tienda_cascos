d6425ff73b80fc6c572a9c8f8ff814310ded5e94
Carlos Rull <carlorullpolo@gmail.com>
2026-02-19 13:12:56 +0100

    Fix email config

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "mihasposssposs@gmail.com",
+    pass: "ogvcvbvyciwjljga"
+  }
+});
+
   auth: {
     user: "mihasposssposs@gmail.com",
     pass: "ogvcvbvyciwjljga"
